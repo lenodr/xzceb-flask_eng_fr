674b4fccc80615018c6703fa34ed3cc5f16d4f69
lenodr <lenodar@gmail.com>
2022-04-30 06:45:16 +0000

    added more tests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,13 +1,17 @@
 import unittest
-from machinetranslation.translator import english_to_french, french_to_english
+from translator import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
-    def test_null_input(self):
+    def test_null_input_english(self):
         self.assertIsNone(english_to_french(None))
+
+    def test_english_to_french_translation(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        
+    def test_null_input_french(self):
         self.assertIsNone(french_to_english(None))
     
-    def test_translation(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    def test_french_to_english_translation(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
 if __name__ == '__main__':
